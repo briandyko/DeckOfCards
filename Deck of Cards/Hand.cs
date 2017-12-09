fa797a2e3eb19d4c6cdf812f456ab9fcd8ba668d
briandyko <briandyko@gmail.com>
2017-12-09 17:05:36 -0500

    mvp

---
+++ Deck of Cards/Hand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deck_of_Cards
+{
+    class Hand
+    {
+        //public int NumberOfCards { get; set; }
+        public int ValueFirstCard { get; set; }
+        //public string SuitFirstCard { get; set; }
+        public int ValueSecondCard { get; set; }
+        //public string SuitSecondCard { get; set; }
+        //public bool IsBusted { get; set; }
+        //public object[] FirstCard { get; set; }
+        //public object[] SecondCard { get; set; }
+        public int Value { get; set; }
+        //public string Suit { get; set; }
+
+
+
+
+
+        public Hand(int valueFirstCard,/* string suitFirstCard,*/ int valueSecondCard /*string suitSecondCard*/)
+        {
+            this.ValueFirstCard = valueFirstCard;
+            //this.SuitFirstCard = suitFirstCard;
+            this.ValueSecondCard = valueSecondCard;
+            //this.SuitSecondCard = suitSecondCard;
+
+        }
+
+        //public Hand(object[] firstCard)
+        //{
+        //    this.FirstCard = firstCard[2];
+        //}
+
+        //public FullDeck(string value, string suit)
+        //{
+        //    this.Value = value;
+        //    this.Suit = suit;
+            
+        //}
+
+
+        public int Hit(int value/*, string suit*/)
+        {
+            int newValue;
+            Random rand = new Random();
+            newValue = value + rand.Next(3, 9);
+
+            return newValue;
+
+                      
+        }
+
+        //public void isBusted()
+        //{
+        //    if (Hand == 21)
+        //    {
+
+        //    }
+        //}
+
+
+    }
+}
--- Deck of Cards/Program.cs
+++ Deck of Cards/Program.cs
@@ -10,8 +10,63 @@ namespace Deck_of_Cards
     {
         static void Main(string[] args)
         {
-            FullDeck testDeck = new FullDeck("3", "clubs")
+            Hand hand = new Hand(3, 2);
 
+            int handTotal = hand.ValueFirstCard + hand.ValueSecondCard;
+            int hitTotal;
+
+            do
+            {
+
+                Random newCard = new Random();
+
+            int hitCard = newCard.Next(1, 10);
+
+            hitTotal = handTotal + hitCard;
+
+            hand.Hit(hitCard);
+            
+                Console.WriteLine(hitTotal);
+
+
+                if (hitTotal > 21)
+                {
+
+                    Console.WriteLine("bust");
+                }
+
+                else
+                {
+                    Console.WriteLine("Would you like another hit?");
+                }
+
+                string response = Console.ReadLine().ToLower();
+
+                if (response == "yes")
+                {
+                    /*Console.WriteLine(*/hitTotal += hand.Hit(hitCard)/*)*/;
+                   
+                    if(hitTotal > 21)
+                        {
+                    Console.WriteLine("real busted");
+                        }
+                    break;
+                }
+                else if (response == "no")
+                        {
+                    Console.WriteLine("Let's see what the dealer's got.");
+                }
+                else
+                {
+                    Console.WriteLine("make a valid choice");
+                }
+
+            }
+            while (hitTotal < 21);
+
+
+            Console.WriteLine("busted");
+        }
         }
     }
-}
+
